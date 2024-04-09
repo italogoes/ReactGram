1b01f84e412e8b91acd6f9c02431cc4bcdfbb142
Italo Goes <italogoes85@gmail.com>
2024-04-08 21:04:13 -0300

    incio do projeto

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+require("dotenv").config()
+require("./config/db.js")
+
+const express = require("express")
+const path = require("path")
+const cors = require("cors")
+const router = require("./routes/Router")
+
+const port = process.env.PORT
+
+const app = express()
+
+// config json and form data response
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+// cors
+app.use(cors({credentials: true, origin: "http://localhost:5000"}))
+
+// upload directory
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
+
+// db connection
+
+
+// routes
+app.use(router)
+
+// run app
+app.listen(port, () => {
+    console.log("app rodando!")
+})
