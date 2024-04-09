d79d10c4c2fa124f5a2b4a3923eb14c3462b2308
Italo Goes <italogoes85@gmail.com>
2024-04-08 21:18:01 -0300

    conexão com o mongo db atlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,20 @@
 const mongoose = require("mongoose")
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASSWORD
 
-// connection
+const conn = async () => {
+    try {
+        
+        const dbConn = await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.tfoxgud.mongodb.net/`)
+
+        console.log("Conectou ao banco!");
+
+        return dbConn
+    } catch (error) {
+        console.log("Erro ao se conectar ao banco de dados...", error)
+    }
+}
+
+conn()
+
+module.exports = conn
