4a1d2ac15b155e81fc7a24f1310dedc2dbeacc6e
Italo Goes <italogoes85@gmail.com>
2024-04-11 00:18:41 -0300

    rota de criacao e login de usuario criadas, alem do token jwt e middleware de seguranca

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -13,9 +13,75 @@ const generateToken = (id) => {
 
 // register user and sign in
 const register = async(req, res) => {
-    res.send("Registro!")
+    const {name, email, password} = req.body;
+
+    // check is user exists
+    const user = await User.findOne({email})
+
+    if (user) {
+        res.status(422).json({errors: ["Por favor, utilize outro e-mail"]})
+        return
+    }
+
+    // Generate password hash
+    const salt = await bcrypt.genSalt()
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    // create user
+    const newUser = await User.create({
+        name,
+        email,
+        password: passwordHash
+    })
+
+    // If user was created successfully, return the token
+    if (!newUser) {
+        res.status(422).json({errors: ["Houve um erro, por favor tente novamente mais tarde."]})
+        return
+    }
+
+    res.status(201).json({
+        _id: newUser._id,
+        token: generateToken(newUser._id)
+    })
+}
+
+const login = async(req, res) => {
+    
+    const {email, password} = req.body;
+
+    const user = await User.findOne({email})
+
+    // check if user exists
+    if (!user) {
+        res.status(404).json({errors: ["Usuário não encontrado."]})
+        return
+    }
+
+    // check if password matches
+    if(!(await bcrypt.compare(password, user.password))) {
+        res.status(422).json({errors: ["Senha incorreta."]})
+        return
+    }
+
+    // return user with token
+    res.status(201).json({
+        _id: user._id,
+        profileImage: user.profileImage,
+        token: generateToken(user._id)
+    })
+}
+
+// get current logged in user
+const getCurrentUser = async (req, res) => {
+
+    const user = req.user
+
+    res.status(200).json(user)
 }
 
 module.exports = {
     register,
+    login,
+    getCurrentUser
 }
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,8 +1,15 @@
 const express = require("express")
 const router = express.Router()
 
-const {register} = require("../controllers/UserController")
+const {register, login, getCurrentUser} = require("../controllers/UserController")
 
-router.post("/register", register)
+// Middlleware
+const validate = require("../middlewares/handleValidation")
+const { userCreateValidation, loginValidation } = require("../middlewares/userValidation")
+const authGuard = require("../middlewares/authGuard")
+
+router.post("/register", userCreateValidation(), validate, register)
+router.post("/login", loginValidation(), validate, login)
+router.get("/profile", authGuard, getCurrentUser)
 
 module.exports = router
