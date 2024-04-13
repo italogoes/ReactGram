f5910bab67fc44c16d6de064a117548524809fca
Italo Goes <italogoes85@gmail.com>
2024-04-13 17:28:50 -0300

    atualizando dados do usuario

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,15 +1,17 @@
 const express = require("express")
 const router = express.Router()
 
-const {register, login, getCurrentUser} = require("../controllers/UserController")
+const {register, login, getCurrentUser, update} = require("../controllers/UserController")
 
 // Middlleware
 const validate = require("../middlewares/handleValidation")
-const { userCreateValidation, loginValidation } = require("../middlewares/userValidation")
+const { userCreateValidation, loginValidation, userUpdateValidation } = require("../middlewares/userValidation")
 const authGuard = require("../middlewares/authGuard")
+const { imageUpload } = require("../middlewares/imageUpload")
 
 router.post("/register", userCreateValidation(), validate, register)
 router.post("/login", loginValidation(), validate, login)
 router.get("/profile", authGuard, getCurrentUser)
+router.put("/", authGuard, userUpdateValidation(), validate, imageUpload.single("profileImage"), update)
 
 module.exports = router
